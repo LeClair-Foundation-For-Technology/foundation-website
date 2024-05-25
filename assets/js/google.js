354421cad8d7ce6ad583b16ccb09968ec9efbe52
Corie LeClair <corie.leclair@snhu.edu>
2024-05-25 19:09:05 -0400

    google tag

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,12 +1,4 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16580207125"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'AW-16580207125');
-</script>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PFF1GYQ5JB"></script>
 <script>
